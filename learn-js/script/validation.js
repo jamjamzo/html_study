841da3dc114c4c2aaaa77533faab611b3b7cf71c
jamjamzo <jaeyoun92@gmail.com>
2024-06-19 18:10:20 +0900

    회원가입 검증 미완

--- learn-js/script/validation.js
+++ learn-js/script/validation.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function () {
   formElem.onsubmit = submitForm;
 });
 
+// alert-message "TEST" 지우기
 function clearMessages() {
   var messages = document.getElementsByClassName('alert-message');
   for (var i = 0; i < messages.length; i++) {
@@ -12,6 +13,17 @@ function clearMessages() {
   }
 }
 
+// 메시지 보여주기?
+function showMessage(inputElement, message) {
+  var messageElem = inputElement.parentNode.querySelector('span');
+  //span에 alert-message를 달아놨기 때문에 경고문장을 표시할 수 있다.
+  messageElem.style.display = 'inline';
+  messageElem.innerText = message;
+
+  inputElement.focus();
+}
+
+// 완성된 폼 제출하기
 function submitForm() {
   //account info
   var accountInput = document.querySelector('input[name="account"]');
@@ -37,6 +49,10 @@ function submitForm() {
   );
   console.log(radioInput.value);
   console.log(checkInput.value);
+
+  if (accountInput.value.length < 6) {
+    showMessage(accountInput, 'Fill out the blank more than 6 words.');
+  }
 
   return false;
 }
