b6c6b18c646ed5ce4f9c2089b58823ac857d5e3f
jamjamzo <jaeyoun92@gmail.com>
2024-06-27 16:45:18 +0900

    인스타그램 카드 완성

--- instagram_sample/scripts/carousel.js
+++ instagram_sample/scripts/carousel.js
@@ -1,17 +1,15 @@
-console.log('hi');
-
 window.addEventListener('load', function () {
   var carousels = document.getElementsByClassName('carousel');
 
   //캐러셀 이벤트를 등록하는 로직
   for (var i = 0; i < carousels.length; i++) {
-    addEventListener(carousels[i]);
+    addEventToCarousel(carousels[i]);
   }
 });
 
-function addEventListener(carouselElem) {
+function addEventToCarousel(carouselElem) {
   var ulElem = carouselElem.querySelector('ul');
-  var liElems = carouselElem.querySelectorAll('li');
+  var liElems = ulElem.querySelectorAll('li');
 
   //너비값 조정
   var liWidth = liElems[0].clientWidth;
@@ -43,34 +41,60 @@ function createListenerSlide(carouselElem) {
 
     // 슬라이드 버튼 체크
     if (
-      clickedButton.classList.contains('slide-right') &&
+      clickedButton.className.includes('right') &&
       currentIndex < liCount - 1
     ) {
       currentIndex++;
       scrollDiv(carouselElem, currentIndex);
-    } else if (
-      clickedButton.classList.contains('slide-left') &&
-      currentIndex > 0
-    ) {
+    } else if (clickedButton.className.includes('left') && currentIndex > 0) {
       currentIndex--;
       scrollDiv(carouselElem, currentIndex);
     }
+    // 인디케이터 업데이트
+    updateIndicator(carouselElem, currentIndex);
+
+    // 슬라이드 버튼 보여줌 업데이트
+    updateSlideButtonVisible(carouselElem, currentIndex, liCount);
+
+    // 새롭게 보여지는 이미지 인덱스 값을 현재 data값으로 업데이트
+    // carouselElem.attributes.data.value = currentIndex;
+    //이 부분에서도 attributes 대신 dataset을 사용해야 합니다:
+    carouselElem.dataset.value = currentIndex;
   };
-
-  // 인디케이터 업데이트
-
-  // 슬라이드 버튼 보여줌 업데이트
-
-  // 새롭게 보여지는 이미지 인덱스 값을 현재 data값으로 업데이트
-  // carouselElem.attributes.data.value = currentIndex;
-  //이 부분에서도 attributes 대신 dataset을 사용해야 합니다:
-  carouselElem.dataset.value = currentIndex;
 }
 
 function scrollDiv(carouselElem, nextIndex) {
-  var scrollable = carouselElem.querySelector('.carousel-container');
+  var scrollable = carouselElem.querySelector('div');
   var liWidth = scrollable.clientWidth;
   var newLeft = liWidth * nextIndex;
 
   scrollable.scrollTo({ left: newLeft, behavior: 'smooth' });
+}
+
+function updateIndicator(carouselElem, currentIndex) {
+  var indicators = carouselElem.querySelectorAll('footer > div');
+  for (var i = 0; i < indicators.length; i++) {
+    if (currentIndex == i) {
+      indicators[i].className = 'active';
+    } else {
+      indicators[i].className = '';
+    }
+  }
+}
+
+function updateSlideButtonVisible(carouselElem, currentIndex, liCount) {
+  var left = carouselElem.querySelector('.slide-left');
+  var right = carouselElem.querySelector('.slide-right');
+
+  if (currentIndex > 0) {
+    left.style.display = 'block';
+  } else {
+    left.style.display = 'none';
+  }
+
+  if (currentIndex < liCount - 1) {
+    right.style.display = 'block';
+  } else {
+    right.style.display = 'none';
+  }
 }
